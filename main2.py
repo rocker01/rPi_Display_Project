bb0b9dfc66c69448aa3f63c147a5758ce237b882
rocker01 <narang.manu@gmail.com>
2020-12-08 11:22:14 -0500

    Latest commit

--- main2.py
+++ main2.py
@@ -35,7 +35,7 @@ ipAddress = socket.gethostbyname(hostName)
 ipAddress2 = commands.getoutput('ifconfig wlan0 | grep "inet " | cut -d " " -f10')
 
 draw.text((0, 0), 'Hello! I\'m ' + hostName, font=font18, fill=0)
-draw.text((0, 35), 'Aditya' + '   ' + ipAddress2, font=font18, fill=0)
+draw.text((0, 35), 'Preeti' + '   ' + ipAddress2, font=font18, fill=0)
 draw.text((0, 70), time.strftime("%H:%M:%S %Y-%m-%d"), font=font18, fill=0)
 
 
