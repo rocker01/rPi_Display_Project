f5d093afd48dedc8caed0f03f74ddab13d9f34f4
rocker01 <narang.manu@gmail.com>
2020-12-08 12:26:29 -0500

    Latest commit

--- main2.py
+++ main2.py
@@ -5,6 +5,8 @@ import sys
 import os
 import socket
 import commands
+import requests
+
 
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
@@ -34,9 +36,25 @@ hostName = socket.gethostname()
 ipAddress = socket.gethostbyname(hostName)
 ipAddress2 = commands.getoutput('ifconfig wlan0 | grep "inet " | cut -d " " -f10')
 
-draw.text((0, 0), 'Hello! I\'m ' + hostName, font=font18, fill=0)
-draw.text((0, 35), 'Preeti' + '   ' + ipAddress2, font=font18, fill=0)
-draw.text((0, 70), time.strftime("%H:%M:%S %Y-%m-%d"), font=font18, fill=0)
+URL = "https://covid.ourworldindata.org/data/owid-covid-data.json"
+
+r = requests.get(url = URL).json()
+
+indCases = int(r['IND']['data'][-1]['total_cases'])
+indNewCases = int(r['IND']['data'][-1]['new_cases'])
+
+canCases = int(r['CAN']['data'][-1]['total_cases'])
+canNewCases = int(r['CAN']['data'][-1]['new_cases'])
+
+usaCases = int(r['USA']['data'][-1]['total_cases'])
+usaNewCases = int(r['USA']['data'][-1]['new_cases'])
+
+draw.text((0, 0), 'IND ' + indCases + ' +' + indNewCases, font=font15, fill=0)
+draw.text((0, 20), 'CAN ' + canCases + ' +' + canNewCases, font=font15, fill=0)
+draw.text((0, 40), 'CAN ' + usaCases + ' +' + usaNewCases, font=font15, fill=0)
+
+
+draw.text((0, 70), time.strftime("%H:%M:%S %Y-%m-%d"), font=font15, fill=0)
 
 
 epd.display(epd.getbuffer(image))
