880eb7b130c3c6ef6886e0bd06854844e207c585
rocker01 <narang.manu@gmail.com>
2020-12-07 19:15:28 -0500

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,97 @@
-# coding=utf-8
-# This is a sample Python script.
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
 
+import logging
+from waveshare_epd import epd2in13_V2
+import time
+from PIL import Image, ImageDraw, ImageFont
+import traceback
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+logging.basicConfig(level=logging.DEBUG)
 
+try:
+    logging.info("epd2in13_V2 Demo")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    epd = epd2in13_V2.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Drawing on the image
+    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+
+    logging.info("1.Drawing on the image...")
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+
+    draw.rectangle([(0, 0), (50, 50)], outline=0)
+    draw.rectangle([(55, 0), (100, 50)], fill=0)
+    draw.line([(0, 0), (50, 50)], fill=0, width=1)
+    draw.line([(0, 50), (50, 0)], fill=0, width=1)
+    draw.chord((10, 60, 50, 100), 0, 360, fill=0)
+    draw.ellipse((55, 60, 95, 100), outline=0)
+    draw.pieslice((55, 60, 95, 100), 90, 180, outline=0)
+    draw.pieslice((55, 60, 95, 100), 270, 360, fill=0)
+    draw.polygon([(110, 0), (110, 50), (150, 25)], outline=0)
+    draw.polygon([(190, 0), (190, 50), (150, 25)], fill=0)
+    draw.text((120, 60), 'e-Paper demo', font=font15, fill=0)
+    draw.text((110, 90), u'微雪电子', font=font24, fill=0)
+    epd.display(epd.getbuffer(image))
+    time.sleep(2)
+
+    # read bmp file
+    logging.info("2.read bmp file...")
+    image = Image.open(os.path.join(picdir, '2in13.bmp'))
+    epd.display(epd.getbuffer(image))
+    time.sleep(2)
+
+    # read bmp file on window
+    logging.info("3.read bmp file on window...")
+    # epd.Clear(0xFF)
+    image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    image1.paste(bmp, (2, 2))
+    epd.display(epd.getbuffer(image1))
+    time.sleep(2)
+
+    # # partial update
+    logging.info("4.show time...")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+
+    epd.init(epd.FULL_UPDATE)
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+
+    epd.init(epd.PART_UPDATE)
+    num = 0
+    while (True):
+        time_draw.rectangle((120, 80, 220, 105), fill=255)
+        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font=font24, fill=0)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if (num == 10):
+            break
+
+    logging.info("Clear...")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    epd.Dev_exit()
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd2in13_V2.epdconfig.module_exit()
+    exit()
